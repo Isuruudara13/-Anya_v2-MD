405b1b46cb65f9ca0802fc9770c043089d103f0a
Isuruudara13 <152253068+Isuruudara13@users.noreply.github.com>
2023-12-02 13:51:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,21 +8,21 @@ global.zApiKey = {// if this API key get expired then please go to https://www.f
 }
 
 
-global.botname = process.env.BotName || "Queen Anya" 
-global.author = process.env.Author || "@PikaBotz" 
-global.packname = process.env.PackName || "Queen Anya v2 MD" 
+global.botname = process.env.BotName || "𝑀𝑅 𝙽𝙰𝙳𝙸𝙻━✗ Bot" 
+global.author = process.env.Author || "@𝑀𝑅 𝙽𝙰𝙳𝙸𝙻━✗" 
+global.packname = process.env.PackName || "𝑀𝑅 𝙽𝙰𝙳𝙸𝙻━✗" 
 global.myweb = process.env.Web || "https://github.com/PikaBotz" 
-global.footer = process.env.Footer || "© Queen Anya Bot" 
-global.prefa = process.env.Prefix || ['-'] 
+global.footer = process.env.Footer || "© 𝑀𝑅 𝙽𝙰𝙳𝙸𝙻━✗" 
+global.prefa = process.env.Prefix || ['.'] 
 global.themeemoji = process.env.ThemeEmoji || "🎐" 
-global.ownername = process.env.Owner_Name || "Pika~Kun" 
-global.ownernumber = process.env.Owner_Number || "916900904828" 
-global.adress = process.env.Continent || "Asia, India, Assam" 
-global.timezone = process.env.TimeZone || "Asia/Kolkata" 
+global.ownername = process.env.Owner_Name || "𝑀𝑅 𝙽𝙰𝙳𝙸𝙻━✗" 
+global.ownernumber = process.env.Owner_Number || "94719370832" 
+global.adress = process.env.Continent || "Asia, Sri lanka, Buttala" 
+global.timezone = process.env.TimeZone || "Asia/Colombo" 
 global.instagramId = process.env.Insta || "8.08_only_mine" 
 global.email = process.env.Email_Id || "example@example.com" 
 
-global.mongoUrl = process.env.MongoDB || "Enter"; 
+global.mongoUrl = process.env.MongoDB || "mongodb+srv://Mrnadil:Nadilh123@cluster0.xeoon1y.mongodb.net/"; 
 
 global.warns = process.env.Warns_Limits || 3; 
 
@@ -101,29 +101,29 @@ global.message = {
 },
 
 module.exports = {
-  botname: process.env.BotName || "Queen Anya", 
-  author: process.env.Author || "@PikaBotz",
-  packname: process.env.PackName || "Queen Anya v2 MD",
+  botname: process.env.BotName || "𝑀𝑅 𝙽𝙰𝙳𝙸𝙻━✗", 
+  author: process.env.Author || "@𝑀𝑅 𝙽𝙰𝙳𝙸𝙻━✗",
+  packname: process.env.PackName || "𝑀𝑅 𝙽𝙰𝙳𝙸𝙻━✗ MD",
   myweb: process.env.Web || "https://github.com/PikaBotz",
-  footer: process.env.Footer || "© Queen Anya Bot",
-  prefa: process.env.Prefix || ['-'],
+  footer: process.env.Footer || "© 𝑀𝑅 𝙽𝙰𝙳𝙸𝙻━✗ md",
+  prefa: process.env.Prefix || ['.'],
   themeemoji: process.env.ThemeEmoji || "🎐",
-  ownername: process.env.Owner_Name || "Pika~Kun",
-  ownernumber: process.env.Owner_Number || "916900904828",
-  adress: process.env.Continent || "Asia, India, Assam",
-  timezone: process.env.TimeZone || "Asia/Kolkata", 
+  ownername: process.env.Owner_Name || "𝑀𝑅 𝙽𝙰𝙳𝙸𝙻━✗",
+  ownernumber: process.env.Owner_Number || "94719370832",
+  adress: process.env.Continent || "Asia, Sri lanka, Buttala",
+  timezone: process.env.TimeZone || "Asia/Colombo", 
   instagramId: process.env.Insta || "8.08_only_mine",
   email: process.env.Email_Id || "example@example.com",
   warns: process.env.Warns_Limits || 3,
-  mongoUrl: process.env.MongoDB || "Enter",
+  mongoUrl: process.env.MongoDB || "mongodb+srv://Mrnadil:Nadilh123@cluster0.xeoon1y.mongodb.net/",
   welcome: process.env.Welcome_Msg || '*@$user* joined this group today as $membersth member.\n\n_$prefix welcome off to disable this message._',
   left: process.env.Left_Msg || 'Ex-member *@$user* is no longer available in this group chat.\n\n_$prefix goodbye off to disable this message._',
   promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
   demote: process.env.Demote_Msg || '*@$user* has been demoted to a member in this group.\n\n_$prefix demotem off to disable this message._',
-  sessionId: process.env.SESSION_ID || "Queen Anya", 
+  sessionId: process.env.SESSION_ID || "MzZVRE1_Queen-Anya_tWDI=", 
   auto_read_status : process.env.AUTO_READ_STATUS || "true",
-  alwaysonline: process.env.ALWAYS_ONLINE || "true", 
-  readmessage: process.env.READ_MESSAGE || "true", 
+  alwaysonline: process.env.ALWAYS_ONLINE || "false", 
+  readmessage: process.env.READ_MESSAGE || "false", 
   auto_status_saver: process.env.AUTO_STATUS_SAVER || "true",
   image_1: readFileSync('./lib/Assets/image_1.jpg'), // Thumbnail for allmenu command
   image_2: readFileSync('./lib/Assets/image_2.jpg'), // null image
